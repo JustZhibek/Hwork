54b79e89e098d10a94292405fbf9a87ac0d88a1a
justZhibek <kamilovazhibek26@gmail.com>
2022-12-18 17:26:27 +0600

    'Hw4'

--- home/HomeWork 4.py
+++ home/HomeWork 4.py
@@ -1,28 +1,21 @@
 class Mammals:
     def __init__(self, name):
         self.name = name
-
-
 class Lion:
     def __init__(self, age):
         self.age = age
-
-
-class Cat(Mammals):
+class Cat:
     def tail(self):
         print('хвостик')
-
-
-class Dog(Lion):
-
-
+class Dog:
     def teeth(self):
         print('зубки')
+class Wolf(Mammals,Lion,Cat, Dog):
+    def __init__(self, name, age):
+         Mammals.__init__(self, name)
+         Lion.__init__(self, age)
 
 
-class Wolf(Cat, Dog):...
-
-
-w=Wolf('Balto')
+w = Wolf('Balto', 18)
 w.tail()
 w.teeth()
